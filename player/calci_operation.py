5ea81baee90ae3a2afdb4975dcb31df6d96e8d9f
kaviyasriprabhakaran <36330090+kaviyasriprabhakaran@users.noreply.github.com>
2018-05-28 12:59:34 +0530

    Create calci_operation.py

---
+++ player/calci_operation.py
@@ -0,0 +1,30 @@
+a=float(input("enter the number:"))
+b=float(input("enter the number:"))
+i=float(input("select number:"))
+
+
+if(i>=1 and i<=1.9):
+  c=a+b
+  print (c)
+elif(i==2):
+  d=a-b
+  print(d)
+elif(i==3):
+  e=a*b
+  print(e)
+elif(i==4):
+  f=a/b
+  print(f)
+elif(i==5):
+  g=a%b
+  print(g)
+else:
+  h=a^b
+  print(h)     
+
+
+
+
+
+ 
+         
